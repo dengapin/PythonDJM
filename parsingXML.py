b5d6aee15bf8ca153e6300fe4dcb8b7949cef7a9
Dennise Pintado <dengab_19@hotmail.com>
2014-01-21 09:23:33 -0500

    Creando repositorio y probando lectura en python

---
+++ parsingXML.py
@@ -0,0 +1,22 @@
+
+def creartxt():
+    archi=open('datos.txt','w')
+    archi.close()
+
+def grabartxt():
+    archi=open('datos.txt','a')
+    archi.write('Linea 1\n')
+    archi.write('Linea 2\n')
+    archi.write('Linea 3\n')
+
+def leertxt():
+        archi=open('datos.txt','r')
+        for linea in archi.readlines(): 
+             print (linea )
+        archi.close() 
+
+     
+creartxt()
+grabartxt()
+leertxt()
+
