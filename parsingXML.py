d329950b5e934ff8ff776be6a51b56bc762bd7a2
Dennise Pintado L. <dengab_19@hotmail.com>
2014-01-25 01:15:01 -0500

    Formando el arbol de parseo

--- parsingXML.py
+++ parsingXML.py
@@ -7,13 +7,26 @@ def leer():
        while linea!="":
               linea=archi.readline()
               contador +=1
-              tomar(contador,linea.strip())
+              parsear(contador,linea.strip())
 
 def parsear(contador,linea):
-        linea = re.sub('[<>/]', '', linea)
-        print(str(contador)+str(linea))
-        
-        
-     
+        linea = re.sub('[/<>"=]', '', linea)
+        atributos = linea.split(' ')
+        tag=[]
+        if 'device' in linea:
+               if len(linea)>6:
+                      for i in range(4):
+                             if i==1:
+                                    a=atributos[i].replace("id_device",'')
+                                    tag.append(a)
+                             if i==2:
+                                    b=atributos[i].replace("user_agent",'')
+                                    bb= b.replace(" ",'_')
+                                    tag.append(bb)
+                             if i==3:
+                                    c=atributos[i].replace("fall_back",'')
+                                    tag.append(c)
+                      print(tag)
+               
+         
 leer()
-
