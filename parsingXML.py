b1a90b58d40a0c1127a9d53332a31c2fd2651817
Jonathan <razerjon24@gmail.com>
2014-02-15 18:36:12 -0500

    Query para buscar id por substrings

--- parsingXML.py
+++ parsingXML.py
@@ -1,3 +1,4 @@
+#!/usr/share/env python3
 import re
 
 '''
@@ -7,11 +8,22 @@ Integrantes: Dennise Pintado
              Janina Costa
              Jonathan Mendieta
 '''
+
+#Querie mostrar devices en los que el fall_back sea un string especifico
+#              if len(atributos)==7:
+#               listaDevice=[]
+#               for i in range(7):
+#                          if i==5:
+#                             fall_back=atributos[i]
+#                             if fall_back=="nokia_6681_ver1":
+#                                listaDevice.append(fall_back)
+#                                print(listaDevice)
  
 def main():
-    archi=open('wurf.xml','r')
+    archi=open('test.xml','r')
     linea=archi.readline()
     cont=0
+    arbolito=[]
     #El archivo se lee hasta que ya no encuentre mas lineas en el xml
     while linea!="":
            linea=archi.readline()
@@ -20,7 +32,6 @@ def main():
            lineajunta= linea
            #Se separan por comillas la lista que correspondera a los atributos
            atributos = linea.split('"')
-           arbolito=[]
            #Si la dimension de la lista es la indicada correspondera sin duda al grupo mencionado
            if len(atributos)==7:
                listaDevice=[]
@@ -34,15 +45,6 @@ def main():
                           if i==5:
                              fall_back=atributos[i]
                              listaDevice.append(fall_back)
-           '''Querie mostrar devices en los que el fall_back sea un string especifico
-              if len(atributos)==7:
-               listaDevice=[]
-               for i in range(7):
-                          if i==5:
-                             fall_back=atributos[i]
-                             if fall_back=="nokia_6681_ver1":
-                                listaDevice.append(fall_back)
-                                print(listaDevice) '''
            elif len(atributos)==3:
                listaGroup=[]
                for i in range(3):
@@ -60,12 +62,37 @@ def main():
                                     listaCapability.append(value)
                listaGroup.append(listaCapability)
                listaDevice.append(listaGroup)
-           #El device terminara cuando encuentre el tag de cierre de Device    
+           #El device terminara cuando encuentre el tag de cierre de Device
            if len(linea)==12:
                cont=cont+1
                print("*****-----------------------------Device"+" "+str(cont)+"-----------------------*******")
                print(listaDevice) 
-               
+               arbolito.append(listaDevice)
+    print searchStringinID(arbolito,["sony","sub"])
+
+##Funciones para buscar Query por ID
+#---------------------------------------------------------------
+#Funcion para analizar el query de encontrar substrings en el ID
+def searchStringinID(deviceList,idSubstringList):
+	devicesFoundList = []
+	for device in deviceList:
+		if stringContainsStrings(device[0],idSubstringList):
+			devicesFoundList.append(device)
+	return devicesFoundList
+
+#Funcion que verifica si existen substrings en el string dado, devuelve Verdadero si los substring son encontrados y Falso si no lo son.
+def stringContainsStrings(deviceID,stringList):
+	boolTester = []
+	for i in range (0,len(stringList)):
+		if stringList[i] in deviceID:
+			boolTester.append(True)
+		else:
+			boolTester.append(False)
+	for j in range (0,len(boolTester)):
+		if boolTester[j] == False:
+			return False
+	return True
+#---------------------------------------------------------------	
 
 if __name__ == '__main__':
     main()
